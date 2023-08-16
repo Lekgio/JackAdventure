35fc6e158c97eab6b2d8b1da9d81921bf7e640a9
Doanmaiha <doanmaiha1502@gmail.com>
2023-08-16 14:53:39 +0700

    Animation

--- JackAdventue/JackAdventue.cpp
+++ JackAdventue/JackAdventue.cpp
@@ -1,27 +1,29 @@
 #include <SFML/Graphics.hpp>
+#include "Animation.h"
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-    sf::Sprite shape;
+    sf::RenderWindow window(sf::VideoMode(400, 400), "JackAdventure");
+    sf::Texture tex;
+    tex.loadFromFile("DinoSprites.png");
+    Animation ani(tex, sf::Vector2i(6,1), 0.1f);
+    ani.setScale(2.f, 2.f);
 
-    {
-        sf::Texture texture;
-        texture.loadFromFile("../Data/Textures/HB.png");
-        shape.setTexture(texture);
-    }
-    
+    sf::Clock clock;
+    float deltaTime = 0.f;
     while (window.isOpen())
     {
+        deltaTime = clock.restart().asSeconds();
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
+        ani.Update(deltaTime);
         window.clear(sf::Color::White);
-        window.draw(shape);
+        window.draw(ani);
         window.display();
     }
 
